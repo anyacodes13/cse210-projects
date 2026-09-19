a0ec174ea1071410d020ed4e5bf4ae59c1a86600
anyacodes13 <miss.anyakay@gmail.com>
2026-09-19 17:56:56 -0600

    finished of prove 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Asking the user fore their grade
+        Console.WriteLine("What is you grade in this class?");
+        int grade = 
+        int.Parse(Console.ReadLine());
+        string letter = "";
+
+        // Print in the console the grade
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        // Print if the user is passing 
+        if (grade >= 70)
+        {
+            Console.WriteLine("You are passing the class. Congrats!!");
+        }
+
+        else
+        {
+            Console.WriteLine("You did not pass. Try again! You got this!");
+        }
+
+
+
+
     }
 }
